3cb738c97efcabb64bd92d90b0a98bdb8590295b
Arijeet2580 <kararijeet26@gmail.com>
2022-10-18 23:30:53 +0530

    Added tab to prinf statement

--- Area of a circle.c
+++ Area of a circle.c
@@ -2,9 +2,9 @@
 #include<math.h>
 int main(){
 	float radius, area;
-	printf("Enter the radius of the circle:");
+	printf("Enter the radius of the circle:\t");
 	scanf("%f",&radius);
-	printf("The area of the circle is:");
+	printf("The area of the circle is:\t");
 	area=3.14*pow(radius,2);
 	printf("%f",area);
 }
