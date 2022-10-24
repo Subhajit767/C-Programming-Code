8bb3777c68381779a5acb9d4e335dff3a071912a
Anirban6756 <anirbandebnath366@gmail.com>
2022-10-25 00:01:44 +0530

    Happy coding

--- Greatest of three numbers.c
+++ Greatest of three numbers.c
@@ -2,7 +2,7 @@
 int main(){
 	int a,b,c,d;
 	printf("Enter the value of three numbers:");
-	scanf("%d%d%d",&a,&b,&c);
+	scanf("%d%d%d",&a,&b,&c);//Accepting 3 numbers from the user
 	d=(a>b)&&(a>c)?a:((b>c)?b:c);
 	printf("The greatest of the three numbers is %d",d);
 	return 0;
