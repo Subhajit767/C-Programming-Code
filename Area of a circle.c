a49999c5df702dbd6454943e5b191e34cb2ef71a
Anirban6756 <anirbandebnath366@gmail.com>
2022-10-24 19:22:18 +0530

    This code is now modified

--- Area of a circle.c
+++ Area of a circle.c
@@ -3,6 +3,7 @@
 int main(){
 	float radius, area;
 	printf("Enter the radius of the circle:\t");
+	//Storing the area of the circle by user
 	scanf("%f",&radius);
 	printf("The area of the circle is:\t");
 	area=3.14*pow(radius,2);
